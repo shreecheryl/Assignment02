f12fc15160eb916c47dd8634ecb608cd24c0160b
shreecheryl <shreecheryl@att.net>
2023-02-18 13:34:48 -0800

    finessed Larger or Smaller

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,9 +1,9 @@
 let x = parseInt(prompt("Enter a number"));
 let y = parseInt(prompt("Enter a second number"));
 if (x > y) {
-    document.write(x);
-} else if (y < x) {
-    document.write(y);
+    document.write(`The larger of the two numbers that you entered is ${x}`);
+} else if (y > x) {
+    document.write(`The larger of the two numbers that you entered is ${y}`);
 } else {
     document.write("You entered the same number");
 }
